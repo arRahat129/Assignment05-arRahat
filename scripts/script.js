ddd52fe56b13d9471d1131f43107649e6d39a5b9
arRahat129 <rahashik129@gmail.com>
2026-03-07 23:39:15 +0600

    toogling fixed, all, open, and closed toogles will return the card based on status. and count based on how many childrens are on the container

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,15 @@
+let allIssues = [];
 const allFilterBtn = document.getElementById("all-filter-btn");
 const openFilterBtn = document.getElementById("open-filter-btn");
 const closedFilterBtn = document.getElementById("closed-filter-btn");
 
 const issueCount = document.getElementById("issue-count");
 
+const loadingSpinner = document.getElementById("loading-spinner");
+
 const cardsContainer = document.getElementById("cards-container");
 
-const labelContainer = document.querySelectorAll("#labels");
-
-function toogleStyle(id){
+function toogleStyle(id) {
     allFilterBtn.classList.remove("btn-primary");
     openFilterBtn.classList.remove("btn-primary");
     closedFilterBtn.classList.remove("btn-primary");
@@ -26,65 +27,115 @@ function toogleStyle(id){
 function showLoading() {
     loadingSpinner.classList.remove("hidden");
     loadingSpinner.classList.add("flex");
-    treesContainer.innerHTML = "";
+    cardsContainer.innerHTML = "";
 }
 
 function hideLoading() {
-    loadingSpinner.classList.add("hidden");
     loadingSpinner.classList.remove("flex");
+    loadingSpinner.classList.add("hidden");
 }
 
-function countLength(issues){
-    let allCardsTotal = issues.length;
+function countLength() {
+    let allCardsTotal = cardsContainer.children.length;
     // console.log(allCardsTotal);
     issueCount.innerText = `${allCardsTotal} Issues`
-    
 }
 
 
 async function loadAllIssues() {
+    showLoading();
     const response = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
     const data = await response.json();
-    const issues = data.data;
+    allIssues = data.data;
+
+    // console.log((allIssues));
     
-    displayCards(issues);
-    countLength(issues);
+    
+    hideLoading();
+    displayCards(allIssues);
+    countLength();
     
 }
 
-function issueLabels(labels, container){
+async function loadOpenIssues(){
+    showLoading();
+
+    if(allIssues.length === 0){
+        await loadAllIssues();
+    }
+    
+    const openIssues = allIssues.filter(issue => issue.status === "open");
+    
+    hideLoading();
+
+    displayCards(openIssues);
+    countLength();
+    
+}
+
+async function loadClosedIssues(){
+
+    showLoading();
+
+    if(allIssues.length === 0){
+        await loadAllIssues();
+    }
+
+    const closedIssues = allIssues.filter(issue => issue.status === "closed");
+
+    hideLoading();
+
+    displayCards(closedIssues);
+    countLength(closedIssues);
+}
+
+allFilterBtn.addEventListener("click", () => {
+    displayCards(allIssues);
+    countLength(allIssues);
+}) ;
+
+openFilterBtn.addEventListener("click", () => {
+    loadOpenIssues();
+});
+
+closedFilterBtn.addEventListener("click", () => {
+    loadClosedIssues();
+});
+
+function issueLabels(labels, container) {
     labels.forEach(label => {
         const button = document.createElement("button");
         let icon = "";
-        
-        if(label === "bug"){
+
+        if (label === "bug") {
             button.className = "w-fit bg-[#FEECEC] text-[#EF4444] border border-[#f7c3c3] p-2 rounded-full font-medium text-xs";
             icon = `<i class="fa-solid fa-bug text-[#EF4444]"></i>`;
         }
-        else if(label === "help wanted"){
+        else if (label === "help wanted") {
             button.className = "bg-[#FFF8DB] text-[#D97706] border border-[#FDE68A] p-2 rounded-full font-medium text-xs";
             icon = `<i class="fa-solid fa-life-ring text-[#D97706]"></i>`;
         }
-        else if(label === "enhancement"){
+        else if (label === "enhancement") {
             button.className = "bg-[#DEFCE8] text-[#00A96E] border border-[#BBF7D0] p-2 rounded-full font-medium text-xs";
             icon = `<i class="fa-regular fa-star text-[#00A96E]"></i>`;
         }
-        else{
+        else {
             button.className = "hidden";
         }
 
         button.innerHTML = `
-            ${icon} ${label}
+            ${icon} ${label.toUpperCase()}
         `;
 
         container.appendChild(button);
-        
+
     });
 }
 
-function displayCards(issues){
+function displayCards(issues) {
     // console.log(issues);
-    
+    cardsContainer.innerHTML = "";
+
     issues.forEach(issue => {
         const card = document.createElement("div");
         card.classList = "card shadow-md border-t-2 border-t-[#00A96E]"
@@ -96,25 +147,22 @@ function displayCards(issues){
                     </div>
 
                     <div id="priority">
-                        <button id="priority-btn" class="${
-                            issue.priority === "high"
-                            ? "bg-[#FEECEC] text-[#EF4444]"
-                            : issue.priority === "medium"
-                            ? "bg-[#FFF6D1] text-[#F59E0B]"
-                            : issue.priority === "low"
-                            ? "bg-[#EEEFF2] text-[#9CA3AF]"
-                            : ""
-                        } px-8 py-2 rounded-full font-medium text-xs">${issue.priority.toUpperCase()}</button>
+                        <button id="priority-btn" class="${issue.priority === "high"
+                ? "bg-[#FEECEC] text-[#EF4444]"
+                : issue.priority === "medium"
+                    ? "bg-[#FFF6D1] text-[#F59E0B]"
+                    : issue.priority === "low"
+                        ? "bg-[#EEEFF2] text-[#9CA3AF]"
+                        : ""
+            } px-8 py-2 rounded-full font-medium text-xs">${issue.priority.toUpperCase()}</button>
                     </div>
                 </div>
 
                 <div class="space-y-2">
                     <h2 id="title" class="text-[#1F2937] text-sm font-semibold">${issue.title}</h2>
                     <p id="description" class="text-[#64748B] text-xs line-clamp-2">${issue.description}</p>
-                    <div class="flex gap-2">
-                        <span id="labels">
-                            
-                        </span>
+                    <div class="labels flex gap-2">
+                        
                     </div>
                 </div>
             </div>
@@ -122,10 +170,13 @@ function displayCards(issues){
             <hr class="w-full border border-gray-200">
 
             <div class="lower-part space-y-2 p-3">
-                <p id="author" class="text-[#64748B] text-xs">#1 by john_doe</p>
-                <p id="created-at" class="text-[#64748B] text-xs">1/15/2024</p>
+                <p id="author" class="text-[#64748B] text-xs">#${issue.id} by ${issue.author}</p>
+                <p id="created-at" class="text-[#64748B] text-xs">${issue.createdAt}</p>
             </div>
         `;
+
+        const labelContainer = card.querySelector('.labels');
+        issueLabels(issue.labels, labelContainer);
 
         cardsContainer.appendChild(card);
     });
